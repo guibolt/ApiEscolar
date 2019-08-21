e0dd4567063c5bfa5038c068199f56429f0f92d6
guibolt <guilhermedosreiss@gmail.com>
2019-08-21 16:43:00 -0300

    retirando a inteface

---
+++ Core/Interface/ICore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interface
+{
+    public interface ICore<T>
+
+    {
+        T Cadastrar();
+        T BuscarId();
+        T BuscarTodos();
+        void Deletar();
+        T Atualizar();
+
+    }
+}
