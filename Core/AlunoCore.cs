c8c7d339e947782c1dfff44bf80d6d592181e024
DrFaelSan <rafaelvplima@gmail.com>
2019-08-21 16:39:52 -0300

    implementação Metodo Cadastrar ProfressorCore

--- Core/AlunoCore.cs
+++ Core/AlunoCore.cs
@@ -1,35 +1,34 @@
-﻿using Core.Interface;
-using Model;
+﻿using Model;
 using System;
 
 namespace Core
 {
-    public class AlunoCore : ICore<Aluno>
+   public class AlunoCore 
 
-    {
-        public Aluno Atualizar()
-        {
-            throw new NotImplementedException();
-        }
+   {
+       public Aluno Atualizar()
+       {
+           throw new NotImplementedException();
+       }
 
-        public Aluno BuscarId()
-        {
-            throw new NotImplementedException();
-        }
+       public Aluno BuscarId()
+       {
+           throw new NotImplementedException();
+       }
 
-        public Aluno BuscarTodos()
-        {
-            throw new NotImplementedException();
-        }
+       public Aluno BuscarTodos()
+       {
+           throw new NotImplementedException();
+       }
 
-        public Aluno Cadastrar()
-        {
-            throw new NotImplementedException();
-        }
+       public Aluno Cadastrar()
+       {
+           throw new NotImplementedException();
+       }
 
-        public void Deletar()
-        {
-            throw new NotImplementedException();
-        }
-    }
+       public void Deletar()
+       {
+           throw new NotImplementedException();
+       }
+   }
 }
